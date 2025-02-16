d4059e41d6d9ff21911fc50136a6d27992006353
iankalcevic <iankalcevic@usf.edu>
2025-02-16 13:50:43 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -9,3 +9,15 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 calculateSalary(5000, 500, 0.1); //Expected output: $5000.00
 calculateSalary(7000, 1000, 0.15); //Expected output: $6950.00
+
+// Task 2 - Product Price After Discount
+
+const calculateDiscount = function(price, discountRate) {
+
+    const finalPrice = price - (price * discountRate); //Calculating the final price
+
+    console.log('Final Price: $${finalPrice.toFixed(2)}'); //Logging final price
+};
+
+calculateDiscount(100, 0.2); //Final price: $80.00
+calculateDiscount(250, 0.15); //Final price: $212.50
