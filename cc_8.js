8819c2a38112de925c4a5264b1eb9f8a234b4b18
iankalcevic <iankalcevic@usf.edu>
2025-02-16 14:03:20 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -21,3 +21,21 @@ const calculateDiscount = function(price, discountRate) {
 
 calculateDiscount(100, 0.2); //Final price: $80.00
 calculateDiscount(250, 0.15); //Final price: $212.50
+
+// Task 3 - Service Fee Calculation
+
+const calculateServiceFee = (amount, serviceType) => {
+
+    let feeRate = 0; //Initializing the rate to 0
+
+    if (serviceType === "Premium") feeRate = 0.15; //15% for premium
+    else if (serviceType === "Standard") feeRate = 0.10; //10% for standard
+    else if (serviceType === "Basic") feeRate = 0.05; //5% for basic
+
+    const fee = amount * feeRate; //Calculating the fee
+
+    console.log('Service fee: $${fee.toFixed(2)}'); //Log service fee
+};
+
+calculateServiceFee(200, "Premium"); //Expected output: $30.00
+calculateServiceFee(500, "Standard"); //Expected output: $50.00
